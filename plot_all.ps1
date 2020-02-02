bab9e3806e917f44c851f2d10de01c5a768a904e
Nan Ma <nanma@microsoft.com>
2020-02-01 16:03:48 -0800

    move static files

--- plot_all.ps1
+++ plot_all.ps1
@@ -1,6 +1,7 @@
 wolframscript.exe .\plot.wls
 wolframscript.exe .\min_norm_plot.wls
 wolframscript.exe .\edge_first_plot.wls
+wolframscript.exe .\edge_tilt_plot.wls
 wolframscript.exe .\perspective_edge.wls
 wolframscript.exe .\perspective_cell.wls
 git add .
