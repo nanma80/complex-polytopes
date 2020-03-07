54867cbab046d6f082a7f0b8bd95cbba40182130
Nan Ma <nanma@microsoft.com>
2020-03-06 17:33:47 -0800

    regenerate perspective plots

--- plot_all.ps1
+++ plot_all.ps1
@@ -1,9 +1,9 @@
-wolframscript.exe .\plot.wls
+# wolframscript.exe .\plot.wls
 # wolframscript.exe .\min_norm_plot.wls
 # wolframscript.exe .\edge_first_plot.wls
 # wolframscript.exe .\edge_tilt_plot.wls
-# wolframscript.exe .\perspective_edge.wls
-# wolframscript.exe .\perspective_cell.wls
+wolframscript.exe .\perspective_edge.wls
+wolframscript.exe .\perspective_cell.wls
 git add .
-git commit -m "regenerate vertex first plot with a diff orientation"
+git commit -m "regenerate perspective plots"
 git push
